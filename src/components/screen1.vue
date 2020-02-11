7ea5062b5eb135b4110d8d1c726239525fe16f0e
superPeltos <tibo.pelletier@gmail.com>
2020-02-11 01:10:52 +0100

    add vue on face api

---
+++ src/components/screen1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods:{
+    increment(){
+      console.log(this.$store.state.count);
+      this.$store.commit("increment")
+    }
+  }
+}
+</script>
+
---
+++ src/components/screen2.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="screen">
+        Votre nom est : {{name}}
+        <input id="name" type="text" v-model="name">
+        <button @click="increment">+</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'screen1',
+    data: function () {
+      return {
+        name: null,
+      }
+    },
+    methods: {
+      increment() {
+        this.$store.dispatch("setName", {
+          name: 'test'
+        });
+        console.log(this.$store.state.name);
+      }
+    }
+  }
+</script>
+
